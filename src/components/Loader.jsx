eebc27647467e3d095b159bac22064397fcfc903
Sunilshah-7 <sunilshah2416@gmail.com>
2024-01-31 15:13:07 +0545

    3d iphone landing page

---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AnimatedLogo from "../assets/images/logo-animated.gif";
+
+export default function Loader() {
+  return <div className="loader">
+    <img className="logo" src={AnimatedLogo} alt="iphone logo" />
+  </div>;
+}
